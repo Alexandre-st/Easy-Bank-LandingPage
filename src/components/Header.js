e48672f645d9fd68689a94c3f01ef9287d413cf2
Alexandre-st <alexandre.saintprix@gmail.com>
2021-08-30 16:44:27 +0200

    Add the header and hero part only mobile design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// == IMPORT IMAGES
+import logo from '../assets/images/logo.svg';
+import hamburger from '../assets/images/icon-hamburger.svg';
+import closeMenu from '../assets/images/icon-close.svg';
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleClick = () => {
+    if (menu === false) {
+      setMenu(true);
+    } else {
+      setMenu(false);
+    };
+  };
+
   return ( 
     <header>
-      <div className="container">
-        <a href="/"><img alt="EasyBank"></img></a>
-        <nav className="navigation desktop">
+      <div className="navigation">
+      <div className="navigation-logo">
+        <a href="/">
+          <img src={logo} alt="EasyBank"></img>
+        </a>
+      </div>
+        <nav className="navigation-desktop">
           <ul>
             <li>Home</li>
             <li>About</li>
@@ -15,6 +34,31 @@ const Header = () => {
           </ul>
           <a href="/" className="button">Request Invite</a>
         </nav>
+
+        {/* / == MENU == / */}
+        <div className="navigation-menu" onClick={handleClick}>
+          {!menu && 
+            <div>
+              <img className="navigation-toggle" src={hamburger} alt="Open for navigation" />
+            </div>
+          }
+          {menu &&
+          <div>
+            <img className="navigation-toggle" src={closeMenu} alt="Close the navigation" />
+            <div className="navigation-background">
+              <nav className="navigation-mobile">
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                  <li>Blog</li>
+                  <li>Carrers</li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          }
+        </div>
       </div>
     </header>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
+// == IMPORT
+import phones from '../assets/images/image-mockups.png';
+
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="container">
-        <div className="hero-content">
-          <h1 className="hero-title">Next generation digital banking</h1>
-          <p className="hero-text">Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
-          <a href="/" className="button">Request Invite</a>
-        </div>
+      <div className="hero-image">
+        <img src={phones} alt="Background Phone" />
+      </div>
+      <div className="hero-content">
+        <h1 className="hero-content-title">Next generation digital banking</h1>
+        <p className="text">Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
+        <a href="/" className="button">Request Invite</a>
       </div>
     </section>
   );
-}
+};
  
 export default Hero;

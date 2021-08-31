95e3271319872c9266e0d5953cdfac09c7707481
Alexandre-st <alexandre.saintprix@gmail.com>
2021-08-31 16:08:55 +0200

    Add styles for two components only mobile-design

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,49 +1,55 @@
 import React from 'react';
 
+// == IMPORT IMAGES
+import dollars from '../assets/images/image-currency.jpg';
+import restaurant from '../assets/images/image-restaurant.jpg';
+import plane from '../assets/images/image-plane.jpg';
+import confetti from '../assets/images/image-confetti.jpg';
+
 const Articles = () => {
 
   const Article = (props) => {
     return (
-      <article className="article-content">
-        {/* <img src={props.image} alt="Illustration" /> */}
-        <p className="article-content-name">{props.name}</p>
-        <h4>{props.title}</h4>
-        <p className="article-content-text">{props.paragraph}</p>
+      <article className="article">
+        <img src={props.image} alt="Illustration" />
+        <div className="article-content">
+          <p className="article-content-name">{props.name}</p>
+          <h4>{props.title}</h4>
+          <p className="article-content-text">{props.paragraph}</p>
+        </div>
       </article>
     )
   };
 
   return (
     <section className="articles">
-      <div className="container">
-        <h2>Latest Articles</h2>
+      <h2>Latest Articles</h2>
 
-        <div className="articles article">
-          <Article 
-            // image=""
-            name="By Claire Robinson"
-            title="Receive money in any currency with no fees"
-            paragraph="The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …"
-          />
-          <Article 
-            // image=""
-            name="By Wilson Hutton"
-            title="Treat yourself without worrying about money"
-            paragraph="Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …"
-          />
-          <Article 
-            // image=""
-            name="By Wilson Hutton"
-            title="Take your Easybank card wherever you go"
-            paragraph="We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …"
-          />
-          <Article 
-            // image=""
-            name="By Claire Robinson"
-            title="Our invite-only Beta accounts are now live!"
-            paragraph="After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ..."
-          />
-        </div>
+      <div>
+        <Article 
+          image={dollars}
+          name="By Claire Robinson"
+          title="Receive money in any currency with no fees"
+          paragraph="The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …"
+        />
+        <Article 
+          image={restaurant}
+          name="By Wilson Hutton"
+          title="Treat yourself without worrying about money"
+          paragraph="Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …"
+        />
+        <Article 
+          image={plane}
+          name="By Wilson Hutton"
+          title="Take your Easybank card wherever you go"
+          paragraph="We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …"
+        />
+        <Article 
+          image={confetti}
+          name="By Claire Robinson"
+          title="Our invite-only Beta accounts are now live!"
+          paragraph="After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ..."
+        />
       </div>
     </section>
   );
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,48 +1,52 @@
 import React from 'react';
 
+// == IMPORT IMAGES 
+import banking from "../assets/images/icon-online.svg";
+import budgeting from "../assets/images/icon-budgeting.svg";
+import onboarding from "../assets/images/icon-onboarding.svg";
+import api from "../assets/images/icon-api.svg";
+
 const Description = () => {
   
   const Article = (props) => {
     return (
       <div className="description-content-article">
-        {/* <img src={props.image} alt="logo" /> */}
+        <img className="description-content-article-image" src={props.image} alt="logo" />
         <h3>{props.title}</h3>
-        <p>{props.text}</p>
+        <p className="text">{props.text}</p>
       </div>
     );
   };
 
   return (
     <section className="description">
-      <div className="container">
-        <div className="description-header">
-          <h2>Why choose EasyBank?</h2>
-          <p>
-            We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.
-          </p>
-        </div>
-        <div className="description-content">
-          <Article 
-            // image=""
-            title="Online Banking"
-            text="Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
-         />
-          <Article 
-            // image=""
-            title="Simple Budgeting"
-            text="See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."
-          />
-          <Article 
-            image=""
-            title="Fast Onboarding"
-            text="We don’t do branches. Open your account in minutes online and start taking control of your finances right away."
-          />
-          <Article 
-            image=""
-            title="Open API"
-            text="Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
-          />
-        </div>
+      <div className="description-header">
+        <h2>Why choose EasyBank?</h2>
+        <p className="text">
+          We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.
+        </p>
+      </div>
+      <div className="description-content">
+        <Article 
+          image={banking}
+          title="Online Banking"
+          text="Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
+        />
+        <Article 
+          image={budgeting}
+          title="Simple Budgeting"
+          text="See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."
+        />
+        <Article 
+          image={onboarding}
+          title="Fast Onboarding"
+          text="We don’t do branches. Open your account in minutes online and start taking control of your finances right away."
+        />
+        <Article 
+          image={api}
+          title="Open API"
+          text="Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
+        />
       </div>
     </section>
   );

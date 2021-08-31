2449b0c88eab2b46930847109628a910fc091307
Alexandre-st <alexandre.saintprix@gmail.com>
2021-08-31 17:40:59 +0200

    Mobile design done next => responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,41 @@
 import React from 'react';
 
+// == IMPORT IMAGES
+import logo from '../assets/images/white_logo.svg';
+import facebook from '../assets/images/icon-facebook.svg';
+import youtube from '../assets/images/icon-youtube.svg';
+import twitter from '../assets/images/icon-twitter.svg';
+import pinterest from '../assets/images/icon-pinterest.svg';
+import instagram from '../assets/images/icon-instagram.svg';
+
 const Footer = () => {
   return (
     <section className="footer">
-      <div className="container">
-        <div className="footer-social">
-          <img alt="Logo easybank" />
-          <div className="footer-social-logo">
-            <img alt="Facebook" />
-            <img alt="Youtube" />
-            <img alt="Twitter" />
-            <img alt="Pinterest" />
-            <img alt="Intagram" />
-          </div>
+      <div className="footer-social">
+        <img className="footer-social-logo" src={logo} alt="Logo easybank"  />
+        <div className="footer-social-link">
+          <img alt="Facebook" src={facebook} />
+          <img alt="Youtube" src={youtube} />
+          <img alt="Twitter" src={twitter} />
+          <img alt="Pinterest" src={pinterest} />
+          <img alt="Intagram" src={instagram} />
         </div>
-        <div className="footer-link">
-          <ul>
-            <li>About Us</li>
-            <li>Contact</li>
-            <li>Blog</li>
-          </ul>
-          <ul>
-            <li>Careers</li>
-            <li>Support</li>
-            <li>Privacy Policy</li>
-          </ul>
-        </div>
-        <div className="footer-copyright">
-          <a href="/" className="button">Request Invite</a>
-          <p>© Easybank. All Rights Reserved</p>
-        </div>
+      </div>
+      <div className="footer-link">
+        <ul>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Blog</li>
+        </ul>
+        <ul>
+          <li>Careers</li>
+          <li>Support</li>
+          <li>Privacy Policy</li>
+        </ul>
+      </div>
+      <div className="footer-copyright">
+        <a href="/" className="button">Request Invite</a>
+        <p>© Easybank. All Rights Reserved</p>
       </div>
     </section>
   );
